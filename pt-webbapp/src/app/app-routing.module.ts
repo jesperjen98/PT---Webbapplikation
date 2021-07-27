a59efa3ba0086a00f1c6ccea8f070f1bd5eabd15
Johan Ehinger <joheh148@student.liu.se>
2021-07-27 18:17:49 +0200

    feature: home page
    Added a home page skeleton and some restructroing of the project.

--- pt-webbapp/src/app/app-routing.module.ts
+++ pt-webbapp/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { MemberPageComponent } from './member-page/member-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MemberPageComponent } from './pages/member-page/member-page.component';
 /**
  * AppRoutingModule
  *
  * @author Johan Ehinger (https://github.com/johanehinger)
  */
 
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
   {
     path: 'login',
     component: LoginPageComponent,
--- pt-webbapp/src/app/app.module.ts
+++ pt-webbapp/src/app/app.module.ts
@@ -4,14 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MemberPageComponent } from './member-page/member-page.component';
-import { NavbarComponent } from './member-page/components/navbar/navbar.component';
-import { CardNavigationComponent } from './member-page/components/card-navigation/card-navigation.component';
+import { MemberPageComponent } from './pages/member-page/member-page.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CardNavigationComponent } from './pages/member-page/components/card-navigation/card-navigation.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageFooterComponent } from './shared/page-footer/page-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginPageComponent, MemberPageComponent, NavbarComponent, CardNavigationComponent],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    MemberPageComponent,
+    NavbarComponent,
+    CardNavigationComponent,
+    HomePageComponent,
+    PageFooterComponent,
+  ],
   imports: [ReactiveFormsModule, BrowserModule, NgbModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
