75858795649dad9ee2e37e04c07d34b262baf4a9
Johan Ehinger <joheh148@student.liu.se>
2021-08-30 16:10:35 +0200

    feature navbar
    Added more navbar functionality

--- pt-webbapp/src/app/shared/navbar/navbar.component.ts
+++ pt-webbapp/src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
+import { Roles } from './../../models/roles';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  displayName: string | null = null;
+  // TODO: User should always have a role, however we should check for this regardless. As of now you could in theory be of role null
+  role: Roles | null = null;
+  isSignedIn: boolean = false;
+  isLoaded: boolean = false;
+  constructor(private _authService: AuthService, private _router: Router) {}
   ngOnInit(): void {
+    this._authService.user$.subscribe((user) => {
+      if (user) {
+        this.displayName = user.firstName;
+        this.role = user.role;
+        this.isSignedIn = true;
+      }
+    });
+    this.isLoaded = true;
   }
 
+  public signOut(): void {
+    this._authService.signOut();
+  }
+
+  public signIn(): void {
+    this._router.navigate(['/login']);
+  }
 }
